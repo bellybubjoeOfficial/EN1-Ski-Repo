be098efe73f213af9ffb225ae121116fd754a4aa
thomas-giglia <105068732+thomas-giglia@users.noreply.github.com>
2023-10-18 15:33:35 -0400

    This one has the correct perspective

--- mapTest2.m
+++ mapTest2.m
@@ -8,37 +8,63 @@ m = length(x);
 xr=[x x(1)];
 yr1=y(1)*ones(m+1,1);
 zr=[z z(1)];
-plot3(xr,yr1,zr,'k');
+boxPart1=plot3(xr,yr1,zr,'k');
+boxPart1.Color=[0.7 0.7 0.7];
 hold on
-yr2=y(2)*ones(m+1,1);
-plot3(xr,yr2,zr,'k');
-hold on
-for i=1:1:m
-plot3([x(i) x(i)],y,[z(i) z(i)],'k');    
-end
-hold on
+
+
+slope=patch([-100 100 100 -100],[-5 -5 1205 1210],[0 0 0 0],[0.7 0.7 0.7]);
+skier=patch([0 10 4 6]-5,[5 5 5 5],[0 0 15 15],'k');
+
+
+ax=gca;
+
+disp('axis.CameraPosition');
+disp(ax.CameraPosition);
+disp('axis.CameraTarget');
+disp(ax.CameraTarget);
+disp('axis.CameraViewAngle');
+disp(ax.CameraViewAngle);
+
+ax.CameraPosition=[0 -5 10];
+ax.CameraTarget=[0 5 5];
+%axis.CameraViewAngle=8;
+ax.Projection='perspective';
+grid off
+axis off
+
+%patch(flagXPoints,[605 605 605 605],flagZPoints);
+
+
+xVals=[0 10 10 0 ];
+yVals=[0 0 20 20 ];
+zVals=[0 0 30 30 ];
+%patch(xVals,yVals,zVals,'g');
+
 
 deltaX=10;
 gate.X1=ones(1,12);
 gate.X2=gate.X1+deltaX;
-gate.Y=[1:12]*100+5;
+gate.Y1=[1,2,3,4,5,6,7,8,9,10,11,12]*100+5;
+deltaY=-5;
+gate.Y2=gate.Y1+deltaY;
 deltaZ=10;
-gate.Z1=25;
+gate.Z1=10;
 gate.Z2=gate.Z1+deltaZ;
 deltaLeg=deltaZ*2;  
 
-slope=patch([-100 100 100 -100 -100],[0 0 605 605 0],[15 15 15 15 15],[0.7 0.7 0.7]);
 
 for i=1:12
     gate.X1(i)=50*(-1)^i;
     gate.X2=gate.X1+deltaX;
-    flagXPoints=[gate.X1(i) gate.X2(i) gate.X2(i) gate.X1(i) gate.X1(i)];
-    legXPoints=[gate.X1(i) gate.X1(i)+2 gate.X1(i)+2 gate.X1(i) gate.X1(i)];
-    flagYPoints=[gate.Y(i) gate.Y(i) gate.Y(i) gate.Y(i) gate.Y(i)];
-    flagZPoints=[gate.Z1 gate.Z1 gate.Z2 gate.Z2 gate.Z1];
+    legXPoints=[gate.X1(i) gate.X1(i)+2 gate.X1(i)+2 gate.X1(i)];
+
+    flagXPoints=[gate.X1(i) gate.X2(i) gate.X2(i) gate.X1(i)];
+    flagYPoints=[gate.Y1(i) gate.Y1(i) gate.Y2(i) gate.Y2(i)];
+    flagZPoints=[gate.Z1 gate.Z1 gate.Z2 gate.Z2];
     gate.flag(i)=patch(flagXPoints,flagYPoints,flagZPoints,[1 0.33 0.33]);
-    gate.leftLeg(i)=patch(legXPoints,flagYPoints,(flagZPoints-deltaZ),[1 0.33 0.33]);
-    gate.rightLeg(i)=patch(legXPoints+(deltaX-2),flagYPoints,(flagZPoints-deltaZ),[1 0.33 0.33]);
+    gate.leftLeg(i)=patch(legXPoints,flagYPoints-deltaY,(flagZPoints-deltaZ),[1 0.33 0.33]);
+    gate.rightLeg(i)=patch(legXPoints+(deltaX-2),flagYPoints-deltaY,(flagZPoints-deltaZ),[1 0.33 0.33]);
     if(mod(i,2)==1)
         set(gate.flag(i),'FaceColor',[0.33 0.33 1]);
         set(gate.leftLeg(i),'FaceColor',[0.33 0.33 1]);
@@ -46,23 +72,7 @@ for i=1:12
     end
 end
 
-skier=patch([0 10 10 0 0],[5 5 5 5 5],[15 15 25 25 15],'k');
 
-axis=gca;
-
-disp('axis.CameraPosition');
-disp(axis.CameraPosition);
-disp('axis.CameraTarget');
-disp(axis.CameraTarget);
-disp('axis.CameraViewAngle');
-disp(axis.CameraViewAngle);
-
-%axis.CameraPosition=[10 -645 100];
-%axis.CameraTarget=[0,100,60];
-%axis.CameraViewAngle=8;
-%axis.Projection='perspective';
-
-patch(flagXPoints,[605 605 605 605 605],flagZPoints);
 
 counter=0;
 while(counter<1000)
@@ -73,13 +83,13 @@ while(counter<1000)
         set(gate.leftLeg(i),'YData',get(gate.leftLeg(i),'YData')-1);
         set(gate.rightLeg(i),'YData',get(gate.rightLeg(i),'YData')-1);
         if get(gate.flag(i),'YData')<0
-           set(gate.flag(i),'YData',[605 605 605 605 605])
-           set(gate.rightLeg(i),'YData',[605 605 605 605 605]) 
-           set(gate.leftLeg(i),'YData',[605 605 605 605 605]) 
+           set(gate.flag(i),'YData',get(gate.flag(i),'YData')+1205);
+           set(gate.rightLeg(i),'YData',get(gate.rightLeg(i),'YData')+1205) 
+           set(gate.leftLeg(i),'YData',get(gate.leftLeg(i),'YData')+1205) 
         end
     end
 end
 
-    
+
 
 hold on
