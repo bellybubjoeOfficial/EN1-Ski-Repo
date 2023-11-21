1e9adac144a1ac7cc71210488fac8876e83df0e7
thomas-giglia <105068732+thomas-giglia@users.noreply.github.com>
2023-11-21 12:56:56 -0500

    Add files via upload

--- PhysicsTestWithMap.m
+++ PhysicsTestWithMap.m
@@ -1,5 +1,4 @@
 close
-clear all
 
 global SystemParams force gatePos gateSpeed
 
@@ -20,89 +19,46 @@ xr=[x x(1)];
 yr1=y(1)*ones(m+1,1);
 zr=[z z(1)];
 boxPart1=plot3(xr,yr1,zr,'k');
-boxPart1.Color=[0.7 0.7 0.7];
+boxPart1.Color="none";%[0.7 0.7 0.7];
 hold on
 
 
-slope=patch([-1000 1000 1000 -1000],[-5 -5 1205 1210],[0 0 0 0],[0.7 0.7 0.7]);
-skier=patch([0 10 6 4]-5,[5 5 5 5],[0 0 15 15],'k');
+gameObj.slope=patch([-1000 1000 1000 -1000],[-5 -5 1205 1210],[0 0 0 0],[0.7 0.7 0.7]);
+gameObj.skier=patch([0 10 6 4]-5,[5 5 5 5],[0 0 15 15],'k');
 
 
 ax=gca;
-ax.XLim=[1-myscreensize(3)/2 myscreensize(3)/2];
+ax.XLim=[1-myscreensize(3) myscreensize(3)];
 ax.YLim=[0 myscreensize(4)];
-ax.ZLim = [-250 500];
-speedArray=[];
-timeArray=[];
 
-disp('axis.CameraPosition');
-disp(ax.CameraPosition);
-disp('axis.CameraTarget');
-disp(ax.CameraTarget);
-disp('axis.CameraViewAngle');
-disp(ax.CameraViewAngle);
 
 ax.CameraPosition=[0 -5 10];
 ax.CameraTarget=[0 5 5];
+disp(ax.Projection)
 ax.Projection='perspective';
 grid off
 axis off
 
 
-%{
-r1=1;
-theta1=0:0.01:pi;
-x1=r1*cos(theta1)*21;
-y1=r1*sin(theta1)*21;
-outerSemiCircle=plot3(1.5*x1,x1,(y1)/2-20,'k');
-bottomLine=plot3([-131.5 -68.5], [0 0], [10 10],'k');
-y2=y1*0.7;
-x2=x1*0.7;
-innerSemiCircle=plot3(1.5*x2,x1,y2/2-20,'k');
-ticker=plot3([-100 -100],[171 171],[10 18],'g','LineWidth',2);
 
 
-%numerical information
-minSpeed = 0;
-maxSpeed = 200;
-numOfPoints = 11;
-numbersUnRound = linspace(minSpeed, maxSpeed, numOfPoints);
-numbers = round(numbersUnRound);
-positionOfSpeed = linspace(pi, 0, numOfPoints);
-rad2 = r1 - 70;
-%play around with offset values
-xOffset = x -20;
-yOffset = y+20;
-xPos = rad2 * cos(positionOfSpeed)/4; + xOffset;
-yPos = -rad2 * sin(positionOfSpeed)/4; +yOffset;
-
-for kk = 1:numOfPoints
-    s = strcat(num2str(numbers(numOfPoints-kk+1)));
-    disp(numbers(kk))
-    %mymessage = text(xPos(kk)-1.5, 0, yPos(kk)+1, s, 'FontSize', 10);
-end
-
-startSpeed=1;
-speed=startSpeed;
-maxSpeed=200;
-%}
-
-xVals=[0 10 10 0 ];
-yVals=[0 0 20 20 ];
-zVals=[0 0 30 30 ];
+%xVals=[30 10 30 30]-50;
+%yVals=[10 10 20 20 ];
+%zVals=[10 10 30 30 ];
 %patch(xVals,yVals,zVals,'g');
+gateSpeed=0;
 
-
-gateNum=2;
+Exp.gateNum=12;
+Exp.gateDistance=75;%300;
 deltaX=10;
-gate.X1=ones(1,4);
-gate.X2=gate.X1+deltaX;
-gate.Y1=[1:gateNum]*300;
+gameObj.gate.X1=ones(1,4);
+gameObj.gate.X2=gameObj.gate.X1+deltaX;
+gameObj.gate.Y1=[1:Exp.gateNum]*Exp.gateDistance;
 deltaY=-5;
-gate.Y2=gate.Y1+deltaY;
+gameObj.gate.Y2=gameObj.gate.Y1+deltaY;
 deltaZ=10;
-gate.Z1=10;
-gate.Z2=gate.Z1+deltaZ;
+gameObj.gate.Z1=10;
+gameObj.gate.Z2=gameObj.gate.Z1+deltaZ;
 deltaLeg=deltaZ*2;
 
 %ode params
@@ -111,128 +67,124 @@ time_force = tspan;
 SystemParams.tspan = tspan;
 SystemParams.time_force = time_force;
 SystemParams.Inertia = 1.0;
-SystemParams.Damping = 5;
+SystemParams.Damping = 50;
 SystemParams.y0 = [0 0];
 force = 0;
 
 
-for i=1:gateNum
-    gate.X1(i)=50*(-1)^i;
-    gate.X2=gate.X1+deltaX;
-    legXPoints=[gate.X1(i) gate.X1(i)+2 gate.X1(i)+2 gate.X1(i)];
+for i=1:Exp.gateNum%round(Exp.gateNum/2)
+    gameObj.gate.X1(i)=50*(-1)^i;
+    gameObj.gate.X2=gameObj.gate.X1+deltaX;
+    gameObj.legXPoints=[gameObj.gate.X1(i) gameObj.gate.X1(i)+2 gameObj.gate.X1(i)+2 gameObj.gate.X1(i)];
 
-    flagXPoints=[gate.X1(i) gate.X2(i) gate.X2(i) gate.X1(i)];
-    flagYPoints=[gate.Y1(i) gate.Y1(i) gate.Y2(i) gate.Y2(i)];
-    flagZPoints=[gate.Z1 gate.Z1 gate.Z2 gate.Z2];
-    gate.flag(i)=patch(flagXPoints,flagYPoints,flagZPoints,[1 0.33 0.33]);
-    gate.leftLeg(i)=patch(legXPoints,flagYPoints-deltaY,(flagZPoints-deltaZ),[1 0.33 0.33]);
-    gate.rightLeg(i)=patch(legXPoints+(deltaX-2),flagYPoints-deltaY,(flagZPoints-deltaZ),[1 0.33 0.33]);
+    gameObj.flagXPoints=[gameObj.gate.X1(i) gameObj.gate.X2(i) gameObj.gate.X2(i) gameObj.gate.X1(i)];
+    gameObj.flagYPoints=[gameObj.gate.Y1(i) gameObj.gate.Y1(i) gameObj.gate.Y2(i) gameObj.gate.Y2(i)];
+    gameObj.flagZPoints=[gameObj.gate.Z1 gameObj.gate.Z1 gameObj.gate.Z2 gameObj.gate.Z2];
+    gameObj.gate.flag(i)=patch(gameObj.flagXPoints,gameObj.flagYPoints,gameObj.flagZPoints,[1 0.33 0.33]);
+    gameObj.gate.leftLeg(i)=patch(gameObj.legXPoints,gameObj.flagYPoints-deltaY,(gameObj.flagZPoints-deltaZ),[1 0.33 0.33]);
+    gameObj.gate.rightLeg(i)=patch(gameObj.legXPoints+(deltaX-2),gameObj.flagYPoints-deltaY,(gameObj.flagZPoints-deltaZ),[1 0.33 0.33]);
 
     if(mod(i,2)==1)
-        set(gate.flag(i),'FaceColor',[0.33 0.33 1]);
-        set(gate.leftLeg(i),'FaceColor',[0.33 0.33 1]);
-        set(gate.rightLeg(i),'FaceColor',[0.33 0.33 1]);
+        set(gameObj.gate.flag(i),'FaceColor',[0.33 0.33 1]);
+        set(gameObj.gate.leftLeg(i),'FaceColor',[0.33 0.33 1]);
+        set(gameObj.gate.rightLeg(i),'FaceColor',[0.33 0.33 1]);
     end
 end
 
-counter=0;
+
+
+Exp.counter=0;
 gateSpeed = 1;
-terminalSpeed = 9.8; %tune later
+Exp.terminalSpeed = 9.8; %tune later
 
 %stopwatch message
 xPos7 = myscreensize(3)*(0.1/5);
 yPos7= myscreensize(4)*(0.1/5);
-timerText = text(xPos7, yPos7, -3000, '0', 'FontSize', 20);
-spedometerText = text(xPos7, yPos7, 20, 'j', 'FontSize', 20);
-speedWord = "Speed: ";
+timeMessage = text(xPos7, yPos7, 20.153, '0', 'FontSize', 20);
+speedMessage = text(xPos7+14, yPos7, 15.153, 'j', 'FontSize', 20);
+gatesPassedMessage = text(xPos7+7, yPos7, 18.153, 'j', 'FontSize', 20);
 
 %stopwatch begin
 startTime = tic(); % Start the stopwatch
 elapsedTime = 0;
+Exp.gatesPassed=0;
+speedString= "";
+%outputStates = [elapsedTime,speedString,Exp.gatesPassed];
 
-index = 1;
-bread=0;
-speedArray(1) = 0;
-timeArray(1) = 0;
-while(bread<20)
-    %timer update
-    speedArray(end+1) = gateSpeed;
-    timeArray(end+1) = elapsedTime;
+gatePos_old=gatePos;
+Exp.SkiProtocolCase=0;
+
+while(Exp.gatesPassed<=Exp.gateNum)
     currentTime = toc(startTime);
     elapsedTime = round(currentTime, 2); % Round to 2 decimal places
-    updateTime(timerText, elapsedTime); %update function
-    speedString = num2str(gateSpeed);
-    finalSpeedWord = append(speedWord, speedString);
-    set(spedometerText,'string',finalSpeedWord);
-    counter=counter+1;
-    gateSpeed = gateSpeed + (counter/20000);
-    if (gateSpeed >= terminalSpeed)
-        gateSpeed = terminalSpeed;
+    update_Time(timeMessage, elapsedTime); %update function
+    speedString = strcat("Speed: ",num2str(gateSpeed^2));
+    outputStates = [elapsedTime,speedString,Exp.gatesPassed];
+    set(speedMessage,'string',speedString);
+    set(gatesPassedMessage,'string',strcat('Gates Passed: ',num2str(outputStates(3)),'/12'));
+    %set(gatesPassedMessage,'string',strcat('Force: ',num2str(force)));
+    Exp.counter=Exp.counter+1;
+    gateSpeed = gateSpeed + (Exp.counter/20000);
+    if (gateSpeed >= Exp.terminalSpeed)
+        gateSpeed = Exp.terminalSpeed;
     end
 
     pause(0.01);
-    for i=1:gateNum
-        if(mod(i,2)==1)
-            set(gate.flag(i),'YData',get(gate.flag(i),'YData')-gateSpeed/4);
-            set(gate.leftLeg(i),'YData',get(gate.leftLeg(i),'YData')-gateSpeed/4);
-            set(gate.rightLeg(i),'YData',get(gate.rightLeg(i),'YData')-gateSpeed/4);
-        else
-            set(gate.flag(i),'YData',get(gate.flag(i),'YData')-gateSpeed/1.2/4);
-            set(gate.leftLeg(i),'YData',get(gate.leftLeg(i),'YData')-gateSpeed/1.2/4);
-            set(gate.rightLeg(i),'YData',get(gate.rightLeg(i),'YData')-gateSpeed/1.2/4);
-        end
-
-
+    for i=1:Exp.gateNum
+        
         gateMovements();
 
-        set(gate.flag(i),'XData',get(gate.flag(i),'XData')+gatePos);
-        set(gate.leftLeg(i),'XData',get(gate.leftLeg(i),'XData')+gatePos);
-        set(gate.rightLeg(i),'XData',get(gate.rightLeg(i),'XData')+gatePos);
-
-        if get(gate.flag(i),'YData')<0
-            bread=bread+1;
-            if(bread>5)
-                break
-            end
-            set(gate.flag(i),'YData',zeros(1:4)+900);
-            set(gate.rightLeg(i),'YData',zeros(1:4)+900)
-            set(gate.leftLeg(i),'YData',zeros(1:4)+900)
-        elseif rem(i+2,2) == 1
-            %disp('blue');
-            if mean(get(gate.flag(i),'YData')) <= 5
-                if mean(get(gate.flag(i), 'XData')) <= mean(get(skier, 'XData'))
-                    gateSpeed = gateSpeed - 1.35;
-                    if(gateSpeed<0)
-                        gateSpeed=-gateSpeed;
-                    end
-                    disp('decrease blue');
-                end
-            end
-        elseif rem(i+2,2) == 0
-            %disp('red');
-            if mean(get(gate.flag(i),'YData')) <= 5
-                %disp('y red');
-                if mean(get(gate.flag(i), 'XData')) >= mean(get(skier, 'XData'))
-                    gateSpeed = gateSpeed - 1.35;
-                    if(gateSpeed<0)
-                        gateSpeed=-gateSpeed;
-                    end
-                    disp('decrease red');
-                end
-            end
+        set(gameObj.gate.flag(i),'YData',get(gameObj.gate.flag(i),'YData')-gateSpeed/4);
+        set(gameObj.gate.leftLeg(i),'YData',get(gameObj.gate.leftLeg(i),'YData')-gateSpeed/4);
+        set(gameObj.gate.rightLeg(i),'YData',get(gameObj.gate.rightLeg(i),'YData')-gateSpeed/4);
+        
+        set(gameObj.gate.flag(i),'XData',get(gameObj.gate.flag(i),'XData')+gatePos);
+        set(gameObj.gate.leftLeg(i),'XData',get(gameObj.gate.leftLeg(i),'XData')+gatePos);
+        set(gameObj.gate.rightLeg(i),'XData',get(gameObj.gate.rightLeg(i),'XData')+gatePos);
+        
+  
+        if mean(get(gameObj.gate.flag(i),'YData'))<=5
+             if(mod(i,2) == 1 && mean(get(gameObj.gate.flag(i), 'XData')) <= mean(get(gameObj.skier, 'XData')))
+                gateSpeed = gateSpeed * 0.25;
+             end
+             if(mod(i,2) == 0 && mean(get(gameObj.gate.flag(i), 'XData')) >= mean(get(gameObj.skier, 'XData')))        
+                gateSpeed = gateSpeed * 0.25;
+             end
+         Exp.gatesPassed=Exp.gatesPassed+1;
+            if(Exp.gatesPassed<Exp.gateNum)
+                set(gameObj.gate.flag(i),'YData',zeros(1:4)+(Exp.gateNum*Exp.gateDistance));
+                set(gameObj.gate.rightLeg(i),'YData',zeros(1:4)+(Exp.gateNum*Exp.gateDistance))
+                set(gameObj.gate.leftLeg(i),'YData',zeros(1:4)+(Exp.gateNum*Exp.gateDistance))
+            end 
         end
+        
     end
-    speed=gateSpeed*(200/terminalSpeed);
-    
+    kinemats.speed=gateSpeed*(200/Exp.terminalSpeed);
+
 end
 
-%end game plots
-figure(2)
 
-plot(timeArray, speedArray)
-xlabel('Time (sec)');
-ylabel('Speed (pixels/sec)');
+gameObj.slope.FaceColor="none";
+gameObj.slope.EdgeColor="none";
+gameObj.skier.FaceColor="none";
+gameObj.skier.EdgeColor="none";
+for n=1:Exp.gateNum
+gameObj.gate.flag(n).FaceColor="none";
+gameObj.gate.flag(n).EdgeColor="none";
+gameObj.gate.leftLeg(n).FaceColor="none";
+gameObj.gate.leftLeg(n).EdgeColor="none";
+gameObj.gate.rightLeg(n).FaceColor="none";
+gameObj.gate.rightLeg(n).EdgeColor="none";
+end
+ax.Projection="orthographic";
 
+timeMessage.Position=[-300 40 40];
+speedMessage.Position=[-300 0 0];
+gatesPassedMessage.Position=[-300 -40 -40];
+
+
+
+hold on
 
 
 function gateMovements(~)
