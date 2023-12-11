5cb3c55e424e08cefc32e4f9cbd08926d5bd6183
thomas-giglia <105068732+thomas-giglia@users.noreply.github.com>
2023-12-11 17:06:29 -0500

    Add files via upload

--- mainFile_keyInputs.m
+++ mainFile_keyInputs.m
@@ -1,4 +1,5 @@
 close
+clear
 
 global SystemParams force gatePos gateSpeed
 
@@ -69,10 +70,12 @@ SystemParams.Damping = 50;
 SystemParams.y0 = [0 0];
 force = 0;
 
-speedData=zeros(1,15);
-timeData=zeros(1,15);
-errorData=zeros(1,15);
+speedData=zeros(6,12);
+timeData=zeros(6,12);
+errorData=zeros(6,12);
+errorOverSpeedData=zeros(6,12);
 
+%copied to case 4
 for i=1:Exp.totalGates+2
     gameObj.gate.X1(i)=50*(-1)^i;
     gameObj.gate.X2=gameObj.gate.X1+deltaX;
@@ -105,8 +108,8 @@ speedMessage = text(xPos+14, yPos, 15.153, 'j', 'FontSize', 20,'color','none');
 gatesPassedMessage = text(xPos+7, yPos, 18.153, 'j', 'FontSize', 20,'color','none');
 
 instructions1 = text(-200, 40, -80, 'How to Play:', 'FontSize', 20);
-instructions2 = text(-1480, 40, -130, 'Point the light on the Arduino towards the computer screen, tilt it left and right (like a steering', 'FontSize', 20);
-instructions3 =text(-1450, 40, -180, 'wheel) to turn in those directions. Tilt the Arduino forward to speed up, backwards to slow ', 'FontSize', 20);
+instructions2 = text(-1320, 40, -130, 'Use the left arrow to turn to the left, and the right arrow to turn to the right. You will', 'FontSize', 20);
+instructions3 =text(-1480, 40, -180, 'accelerate automatically, use the up arrow speed up more and the down arrow to slow down', 'FontSize', 20);
 instructions4 =text(-1300, 40, -230, 'Stay to the left of the blue gates and the right of the red gates for the best time.', 'FontSize', 20);
 instructions5 =text(-700, 40, -280, ' Press "SPACE" to start and go as fast a you can!', 'FontSize', 20);
 instructions = [instructions1, instructions2, instructions3, instructions4, instructions5];
@@ -118,157 +121,187 @@ Exp.nextGate=0;
 speedString= "";
 %outputStates = [elapsedTime,speedString,Exp.gatesPassed];
 
-gatePos_old=gatePos;
-Exp.SkiProtocolCase=0;
-Exp.done=false;
 
-while(Exp.done == false)
-    gateMovements();
-    pause(1.000000e-11110100000);
-
-    switch Exp.SkiProtocolCase
-
-        %base case start game
-        case 0
-            if (Exp.nextGate<Exp.totalGates)
-                Exp.nextGate = Exp.nextGate + 1;
-                Exp.SkiProtocolCase = 1;
-            else
-                Exp.SkiProtocolCase = 5;
-
-            end
-        case 1 %updates onscreen text
-            currentTime = toc(startTime);
-            elapsedTime = round(currentTime, 2); % Round to 2 decimal places
-            update_Time(timeMessage, elapsedTime); %update function
-            speedString = strcat("Speed: ",num2str(gateSpeed));
-            %outputStates = [elapsedTime,speedString,Exp.nextGate-1];
-            set(speedMessage,'string',speedString);
-            set(gatesPassedMessage,'string',strcat("Gates Passed: ",num2str(Exp.nextGate-1),'/12'));
-            if(gateSpeed>0)
-                if(mod(Exp.nextGate,2)==1 && max(get(gameObj.gate.rightLeg(Exp.nextGate),'XData')) > 0)         %left of blue (good)
-                    set(idealTrajPatch,'FaceColor','green')
-                    set(idealTrajPatch,'XData',[min(get(gameObj.skier, 'XData')) max(get(gameObj.gate.rightLeg(Exp.nextGate),'XData')) max(get(gameObj.gate.rightLeg(Exp.nextGate),'XData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'XData'))]);
-                elseif  (mod(Exp.nextGate,2)==0 && min(get(gameObj.gate.leftLeg(Exp.nextGate),'XData')) < 0)    %right of red (good)
-                    set(idealTrajPatch,'FaceColor','green')
-                    set(idealTrajPatch,'XData',[max(get(gameObj.skier, 'XData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'XData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'XData')) max(get(gameObj.gate.rightLeg(Exp.nextGate),'XData'))]);
-                elseif mod(Exp.nextGate,2)==1       %right of blue (bad)
-                    set(idealTrajPatch,'XData',[max(get(gameObj.skier, 'XData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'XData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'XData')) max(get(gameObj.gate.rightLeg(Exp.nextGate),'XData'))]);
-                    set(idealTrajPatch,'FaceColor',[1 0.5 0.5]);
-                else                                %left of red (bad)
-                    set(idealTrajPatch,'XData',[min(get(gameObj.skier, 'XData')) max(get(gameObj.gate.rightLeg(Exp.nextGate),'XData')) max(get(gameObj.gate.rightLeg(Exp.nextGate),'XData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'XData'))]);
-                    set(idealTrajPatch,'FaceColor',[1 0.5 0.5]);
-                end
-            end
-
-            if (Exp.nextGate < Exp.totalGates+1)
-                Exp.SkiProtocolCase = 2;
-            else
-                Exp.SkiProtocolCase = 0;
-            end
-        
-        case 2
-            if(gateSpeed<Exp.terminalSpeed)&&(gateSpeed>0)
-                gateSpeed=gateSpeed+(1/200);
-            end
-            gateMovements()
-            
-            if(gateSpeed>0)
-                ax.Projection="perspective";
-                timeMessage.Color='k';
-                speedMessage.Color='k';
-                gatesPassedMessage.Color='k';
-                for a=1:5
-                    instructions(a).Color=[240/255 240/255 240/255];
-                end
-           
-                for i=1:Exp.totalGates                
-                    set(gameObj.gate.flag(i),'YData',get(gameObj.gate.flag(i),'YData')-gateSpeed/4);
-                    set(gameObj.gate.leftLeg(i),'YData',get(gameObj.gate.leftLeg(i),'YData')-gateSpeed/4);
-                    set(gameObj.gate.rightLeg(i),'YData',get(gameObj.gate.rightLeg(i),'YData')-gateSpeed/4);
-                    
-                    set(gameObj.gate.flag(i),'XData',get(gameObj.gate.flag(i),'XData')+gatePos);
-                    set(gameObj.gate.leftLeg(i),'XData',get(gameObj.gate.leftLeg(i),'XData')+gatePos);
-                    set(gameObj.gate.rightLeg(i),'XData',get(gameObj.gate.rightLeg(i),'XData')+gatePos);
-                end
-            end
-
-            set(idealTrajPatch,'YData',[min(get(gameObj.skier, 'YData')) min(get(gameObj.skier, 'YData')) max(get(gameObj.gate.leftLeg(Exp.nextGate),'YData')) max(get(gameObj.gate.leftLeg(Exp.nextGate),'YData'))]);
-            set(idealTrajPatch,'ZData',[min(get(gameObj.skier, 'ZData')) min(get(gameObj.skier, 'ZData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'ZData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'ZData'))]);
-           
-            
-            if max(get(gameObj.gate.leftLeg(Exp.nextGate),'YData'))<=5
-                Exp.SkiProtocolCase = 3;
-                disp("breach breach BREACH")
-            else
-                Exp.SkiProtocolCase = 1;
-            end
-
-
-
-        case 3
-            oldGateSpeed=gateSpeed;
-            error=mean(get(gameObj.gate.flag(Exp.nextGate),'XData'));
-            if mod(Exp.nextGate,2)==0 && error>0
-                error=-abs(error);
-            elseif mod(Exp.nextGate,2)==0
-                error=abs(error); 
-
-            end
-            
-            %potentially move to after data is collected, will ask others - Tommy
-            if(error<0)
-                gateSpeed=gateSpeed*0.9; %change back to *0.25 when done with testing;
-                disp("oops")
-            end
-
-            speedData(Exp.nextGate)=gateSpeed;
-            timeData(Exp.nextGate)=currentTime;
-            errorData(Exp.nextGate)=error;
-            disp("data please "+Exp.nextGate );
-            if(Exp.nextGate<Exp.totalGates)
-                Exp.nextGate=Exp.nextGate+1;
-                Exp.SkiProtocolCase=2;
-            else
-                disp("snipers on roof");
-                Exp.SkiProtocolCase=4;
-            end
-        case 4
-            Exp.nextGate=12;
-            set(gatesPassedMessage,'string',strcat("Gates Passed: ",num2str(Exp.nextGate),'/12'));
-            gateSpeed=0;
-            idealTrajPatch.FaceColor='none';
-            gameObj.slope.FaceColor="none";
-            gameObj.slope.EdgeColor="none";
-            gameObj.skier.FaceColor="none";
-            gameObj.skier.EdgeColor="none";
-            for n=1:Exp.totalGates
-                gameObj.gate.flag(n).FaceColor="none";
-                gameObj.gate.flag(n).EdgeColor="none";
-                gameObj.gate.leftLeg(n).FaceColor="none";
-                gameObj.gate.leftLeg(n).EdgeColor="none";
-                gameObj.gate.rightLeg(n).FaceColor="none";
-                gameObj.gate.rightLeg(n).EdgeColor="none";
-            end
-            ax.Projection="orthographic";
-                    
-            timeMessage.Position=[-300 40 40];
-            gatesPassedMessage.Position=[-300 0 0];
-            speedMessage.Position=[-300 -40 -40];
-            disp(speedData);
-            Exp.done=true;
-            
-            figure(200)
-            hold on
-            bar(speedData);
-            ylabel('Downhill Speed')
-            xlabel('Baseline                                            Eval');
-            %plot(errorData);
-            plot((errorData/10));
-    end
+Data.TrialCounter=1;
+Data.TotalTrials=6;
+for i=1:Data.TotalTrials
+    Exp.SkiProtocolCase=0;
+    Exp.done=false;
     
-end
+    gateSpeed_old=gateSpeed;
+    while(Exp.done == false)
+        if(gateSpeed~=gateSpeed_old)&&(Exp.SkiProtocolCase~=2&&Exp.SkiProtocolCase~=1)
+            disp("gateSpeed: " + gateSpeed);
+        end
+        gateMovements();
+        pause(1.000000e-11110100000);
+    
+        switch Exp.SkiProtocolCase
+    
+            case -1 %reset the game
 
+            
+            case 0             %base case start game
+                if (Exp.nextGate<Exp.totalGates)
+                    Exp.nextGate = Exp.nextGate + 1;
+                    Exp.SkiProtocolCase = 1;
+                else
+                    Exp.SkiProtocolCase = 5;
+    
+                end
+            case 1 %updates onscreen text
+                currentTime = toc(startTime);
+                elapsedTime = round(currentTime, 2); % Round to 2 decimal places
+                update_Time(timeMessage, elapsedTime); %update function
+                speedString = strcat("Speed: ",num2str(gateSpeed));
+                %outputStates = [elapsedTime,speedString,Exp.nextGate-1];
+                set(speedMessage,'string',speedString);
+                set(gatesPassedMessage,'string',strcat("Gates Passed: ",num2str(Exp.nextGate-1),'/12'));
+                if(gateSpeed>0)
+                    if(mod(Exp.nextGate,2)==1 && max(get(gameObj.gate.rightLeg(Exp.nextGate),'XData')) > 0)         %left of blue (good)
+                        set(idealTrajPatch,'FaceColor','green')
+                        set(idealTrajPatch,'XData',[min(get(gameObj.skier, 'XData')) max(get(gameObj.gate.rightLeg(Exp.nextGate),'XData')) max(get(gameObj.gate.rightLeg(Exp.nextGate),'XData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'XData'))]);
+                    elseif  (mod(Exp.nextGate,2)==0 && min(get(gameObj.gate.leftLeg(Exp.nextGate),'XData')) < 0)    %right of red (good)
+                        set(idealTrajPatch,'FaceColor','green')
+                        set(idealTrajPatch,'XData',[max(get(gameObj.skier, 'XData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'XData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'XData')) max(get(gameObj.gate.rightLeg(Exp.nextGate),'XData'))]);
+                    elseif mod(Exp.nextGate,2)==1       %right of blue (bad)
+                        set(idealTrajPatch,'XData',[max(get(gameObj.skier, 'XData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'XData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'XData')) max(get(gameObj.gate.rightLeg(Exp.nextGate),'XData'))]);
+                        set(idealTrajPatch,'FaceColor',[1 0.5 0.5]);
+                    else                                %left of red (bad)
+                        set(idealTrajPatch,'XData',[min(get(gameObj.skier, 'XData')) max(get(gameObj.gate.rightLeg(Exp.nextGate),'XData')) max(get(gameObj.gate.rightLeg(Exp.nextGate),'XData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'XData'))]);
+                        set(idealTrajPatch,'FaceColor',[1 0.5 0.5]);
+                    end
+                end
+    
+                if (Exp.nextGate < Exp.totalGates+1)
+                    Exp.SkiProtocolCase = 2;
+                else
+                    Exp.SkiProtocolCase = 0;
+                end
+            
+            case 2
+                if(gateSpeed<Exp.terminalSpeed)&&(gateSpeed>0)
+                    gateSpeed=gateSpeed+(1);  %change back to +(1/200) after finished testing
+                end
+                gateMovements()
+                
+                if(gateSpeed>0)
+                    ax.Projection="perspective";
+                    timeMessage.Color='k';
+                    speedMessage.Color='k';
+                    gatesPassedMessage.Color='k';
+                    for a=1:5
+                        instructions(a).Color=[240/255 240/255 240/255];
+                    end
+               
+                    for b=1:Exp.totalGates                
+                        set(gameObj.gate.flag(b),'YData',get(gameObj.gate.flag(b),'YData')-gateSpeed/4);
+                        set(gameObj.gate.leftLeg(b),'YData',get(gameObj.gate.leftLeg(b),'YData')-gateSpeed/4);
+                        set(gameObj.gate.rightLeg(b),'YData',get(gameObj.gate.rightLeg(b),'YData')-gateSpeed/4);
+                        
+                        set(gameObj.gate.flag(b),'XData',get(gameObj.gate.flag(b),'XData')+gatePos);
+                        set(gameObj.gate.leftLeg(b),'XData',get(gameObj.gate.leftLeg(b),'XData')+gatePos);
+                        set(gameObj.gate.rightLeg(b),'XData',get(gameObj.gate.rightLeg(b),'XData')+gatePos);
+                    end
+                end
+    
+                set(idealTrajPatch,'YData',[min(get(gameObj.skier, 'YData')) min(get(gameObj.skier, 'YData')) max(get(gameObj.gate.leftLeg(Exp.nextGate),'YData')) max(get(gameObj.gate.leftLeg(Exp.nextGate),'YData'))]);
+                set(idealTrajPatch,'ZData',[min(get(gameObj.skier, 'ZData')) min(get(gameObj.skier, 'ZData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'ZData')) min(get(gameObj.gate.leftLeg(Exp.nextGate),'ZData'))]);
+               
+                
+                if max(get(gameObj.gate.leftLeg(Exp.nextGate),'YData'))<=5
+                    Exp.SkiProtocolCase = 3;
+                    disp("gate "+Exp.nextGate+" passed");
+                else
+                    Exp.SkiProtocolCase = 1;
+                end
+    
+    
+    
+            case 3
+                oldGateSpeed=gateSpeed;
+                error=mean(get(gameObj.gate.flag(Exp.nextGate),'XData'));
+                if error==0
+                    error=0.1;
+                elseif mod(Exp.nextGate,2)==0 && error>0
+                    error=-abs(error);
+                elseif mod(Exp.nextGate,2)==0
+                    error=abs(error); 
+                end
+                
+                %potentially move to after data is collected, will ask others - Tommy
+                if(error<0)
+                    gateSpeed=gateSpeed*0.9; %change back to *0.25 when done with testing;
+                    disp("gate "+Exp.nextGate+" missed");
+                end
+    
+                speedData(Data.TrialCounter,Exp.nextGate)=gateSpeed^2;
+                timeData(Data.TrialCounter,Exp.nextGate)=currentTime;
+                errorData(Data.TrialCounter,Exp.nextGate)=error;
+                errorOverSpeedData(Data.TrialCounter,Exp.nextGate)=error/gateSpeed^2 ;
+                if(Exp.nextGate<Exp.totalGates)
+                    Exp.nextGate=Exp.nextGate+1;
+                    Exp.SkiProtocolCase=2;
+                else
+                    disp("course complete in " +currentTime+ " seconds");
+                    Exp.SkiProtocolCase=4;
+                end
+            case 4
+                disp("gate to move is gate " + Exp.nextGate);
+                set(gameObj.gate.flag(12),'YData',get(gameObj.gate.flag(12),'YData')+600);
+                set(gameObj.gate.leftLeg(12),'YData',get(gameObj.gate.leftLeg(12),'YData')+600);
+                set(gameObj.gate.rightLeg(12),'YData',get(gameObj.gate.rightLeg(12),'YData')+600);
+
+
+                Exp.nextGate=12;
+                set(gatesPassedMessage,'string',strcat("Gates Passed: ",num2str(Exp.nextGate),'/12'));
+                gateSpeed=0;
+                idealTrajPatch.FaceColor='none';
+                
+                for c=1:Exp.totalGates+2
+                    gameObj.gate.X1(c)=50*(-1)^c;
+                    gameObj.gate.X2=gameObj.gate.X1+deltaX;
+                    gameObj.legXPoints=[gameObj.gate.X1(c) gameObj.gate.X1(c)+2 gameObj.gate.X1(c)+2 gameObj.gate.X1(c)];
+                
+                    gameObj.flagXPoints=[gameObj.gate.X1(c) gameObj.gate.X2(c) gameObj.gate.X2(c) gameObj.gate.X1(c)];
+                    gameObj.flagYPoints=[gameObj.gate.Y1(c) gameObj.gate.Y1(c) gameObj.gate.Y2(c) gameObj.gate.Y2(c)];
+                    gameObj.flagZPoints=[gameObj.gate.Z1 gameObj.gate.Z1 gameObj.gate.Z2 gameObj.gate.Z2];
+                    gameObj.gate.flag(c)=patch(gameObj.flagXPoints,gameObj.flagYPoints,gameObj.flagZPoints,[1 0.33 0.33]);
+                    gameObj.gate.leftLeg(c)=patch(gameObj.legXPoints,gameObj.flagYPoints-deltaY,(gameObj.flagZPoints-deltaZ),[1 0.33 0.33]);
+                    gameObj.gate.rightLeg(c)=patch(gameObj.legXPoints+(deltaX-2),gameObj.flagYPoints-deltaY,(gameObj.flagZPoints-deltaZ),[1 0.33 0.33]);
+                
+                    if(mod(c,2)==1)
+                        set(gameObj.gate.flag(c),'FaceColor',[0.33 0.33 1]);
+                        set(gameObj.gate.leftLeg(c),'FaceColor',[0.33 0.33 1]);
+                        set(gameObj.gate.rightLeg(c),'FaceColor',[0.33 0.33 1]);
+                    end
+                end
+                ax.Projection="orthographic";
+                        
+                timeMessage.Position=[-300 -20 -20];
+                gatesPassedMessage.Position=[-300 -60 -60];
+                speedMessage.Position=[-300 -100 -100];
+                disp(speedData);
+                Exp.done=true;
+                
+                figure(200)
+                hold on
+                bar(speedData(Data.TrialCounter,:));
+                bar(errorData(Data.TrialCounter,:)/10);
+                ylabel('Downhill Speed')
+                xlabel('Baseline                                            Eval');
+               
+                plot(errorOverSpeedData(1,:),'LineWidth',4);
+
+
+                if(gateSpeed>0)
+                    Exp.nextGate=1;
+                    Exp.SkiProtocolCase=1;
+                end
+        end
+        
+    end
+end
 
 
 
